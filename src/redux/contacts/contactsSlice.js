577e8be8d49ab4810e4aa370b0fdbf20a052a34d
DonetsYuliya <donecjuli@gmail.com>
2023-02-26 22:23:09 +0200

    creating slice, store, reducer with rt

---
+++ src/redux/contacts/contactsSlice.js
@@ -0,0 +1,30 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const contactsInitialState = [];
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: contactsInitialState,
+  reducers: {
+    addContact: {
+      reducer(state, { payload }) {
+        state.contacts.push(payload);
+      },
+      prepare(name, number) {
+        return {
+          payload: {
+            id: nanoid(),
+            name,
+            number,
+          },
+        };
+      },
+    },
+    deleteContact(state, { payload }) {
+      state.contacts.filter(contact => contact.id !== payload);
+    },
+  },
+});
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+export const contactsReducer = contactsSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,17 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from '@redux-devtools/extension';
-import { rootReducer } from './reducer';
+import { configureStore } from '@reduxjs/toolkit';
+import { contactsReducer } from './contacts/contactsSlice';
+import { filterReducer } from './filters/filterSlice';
 
-const preloadedState = () => {
-  const contacts = JSON.parse(localStorage.getItem('my-contacts'));
-  return contacts
-    ? { contacts: contacts, filter: '' }
-    : { contacts: [], filter: '' };
-};
+// const preloadedState = () => {
+//   const contacts = JSON.parse(localStorage.getItem('my-contacts'));
+//   return contacts
+//     ? { contacts: contacts, filter: '' }
+//     : { contacts: [], filter: '' };
+// };
 
-const enhancer = devToolsEnhancer();
-
-export const store = createStore(rootReducer, preloadedState(), enhancer);
+export const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
+});
